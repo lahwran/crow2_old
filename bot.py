89c5d7fbd8980c60fa9543512b0ac3e63cd1b8e8
lahwran <blendmaster1024@gmail.com>
2011-10-19 09:49:43 -0600

    download exocet if missing

--- bot.py
+++ bot.py
@@ -1,20 +1,60 @@
 #!/usr/bin/python
 
-from system import eventlist, pluginmanager, irc
-from twisted.internet import reactor
-from util import hook, Event
 
-bot = irc.Client()
+exocet_url = "http://launchpad.net/exocet/trunk/0.5/+download/Exocet-0.5.tar.gz"
 
-pluginmanager.loadall()
+def download_exocet():
+    import urllib
+    import tarfile
+    import os
 
-hook.startup.fire(Event(bot = bot))
+    temploc = "exocet_temp.tar.gz"
 
-try:
-    reactor.run()
-finally:
-    hook.shutdown.fire(Event(bot = bot))
-    pluginmanager.unloadall()
+    print "Downloading %s" % exocet_url
+    urllib.urlretrieve(exocet_url, temploc)
+    print "Downloaded to %s" % temploc, "extracting"
+    tar = tarfile.open(temploc, 'r:gz')
+    for item in tar:
+        if item.name.startswith("Exocet-0.5/exocet"):
+            item.name = item.name.replace("Exocet-0.5/", "")
+        else:
+            continue
+        tar.extract(item)
+    print "Extracted, deleting %s" % temploc
+    os.unlink(temploc)
+    print "Done downloading exocet"
+
+def main():
+
+    try:
+        from twisted.internet import reactor
+    except ImportError:
+        print "crow2 depends on twisted!"
+        return
+
+    try:
+        import exocet
+    except ImportError:
+        print "Exocet not found, downloading"
+        download_exocet()
+    from system import eventlist, pluginmanager, irc
+    from util import hook, Event
+
+
+    bot = irc.Client()
+
+    pluginmanager.loadall()
+
+    hook.startup.fire(Event(bot = bot))
+
+    try:
+        reactor.run()
+    finally:
+        hook.shutdown.fire(Event(bot = bot))
+        pluginmanager.unloadall()
+
+if __name__ == "__main__":
+    main()
 
 
 
